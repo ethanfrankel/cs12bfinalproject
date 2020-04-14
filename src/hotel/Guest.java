62090011f0cbb046181e79ff6cfe7b0b25712de3
ethanfrankel <58448173+ethanfrankel@users.noreply.github.com>
2020-04-13 23:43:05 -0400

    abstract class guest

---
+++ src/hotel/Guest.java
@@ -0,0 +1,12 @@
+package hotel;
+
+public class Guest extends Customer {
+	/**
+	 * guests are customers spending the night on the property
+	 */
+
+	public Guest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
