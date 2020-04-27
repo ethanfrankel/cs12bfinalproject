adc3e1ccdfda2a22fd4d56a10175784c4b2a351e
ethanfrankel <58448173+ethanfrankel@users.noreply.github.com>
2020-04-27 17:32:14 -0400

    ef

--- src/hotel/FinancialAnalysis.java
+++ src/hotel/FinancialAnalysis.java
@@ -1,6 +1,14 @@
 package hotel;
 
 public class FinancialAnalysis {
+	
+	double fixedCosts;
+	double employeeSalaries;
+	double hotelRevenue;
+	double restaurantRevenue;
+	double profit;
+	
+	
 
 	public FinancialAnalysis() {
 		// TODO Auto-generated constructor stub
