57b73b8a99810f7c8f75a61b20071c48ed30d9e1
ethanfrankel <58448173+ethanfrankel@users.noreply.github.com>
2020-04-29 01:03:20 -0400

    FinancialAnalysis comments

--- src/hotel/FinancialAnalysis.java
+++ src/hotel/FinancialAnalysis.java
@@ -6,30 +6,32 @@ public class FinancialAnalysis {
 	double totalHotelCosts;
 	double dailyFixedRestaurantCosts = 150.0; //per big tick
 	double totalRestaurantCosts;
-	double staffSalary = 100.0; //per big tick
-	double totalStaffComp; //comp=compensation
-	double explicitCosts;
+	double staffSalary = 100.0; //per big tick per staff member
+	double totalStaffComp; //Comp = compensation
+	double explicitCosts;//All costs
 	double hotelRevenue;
 	double restaurantRevenue;
 	double totalRevenue;
 	double profit;
 	double numSmallTicks;
-	double currentAccountBalance;
+	double currentAccountBalance; //Updated at the end of each day
 	
-	/**
-	 * for current analysis when prompted by user
-	 */
 	
 	public FinancialAnalysis(double initialMoney) {
-		this.currentAccountBalance = initialMoney;
+		this.currentAccountBalance = initialMoney;//passed from hotel
 	}
 	
 	public double dayRevenue(Room[] rooms, Restaurant restaurant) { //add restaurant calcs
+		/**
+		 * Calculates the day's revenue
+		 * hotel room sales
+		 * restaurant sales
+		 */
 		double roomEarnings = 0;
 		double restaurantEarnings = 0;
 		for (int i = 0; i < rooms.length; i++) {
-			if (!rooms[i].available) {
-				roomEarnings = roomEarnings + rooms[i].price;
+			if (!rooms[i].available) {//if room is occupied
+				roomEarnings = roomEarnings + rooms[i].price;//price specific to room type
 			}
 		}
 		//restaurant calcs
@@ -37,29 +39,34 @@ public class FinancialAnalysis {
 		return dayRevenue;
 	}
 
-	public double dayCosts(int numStaff) { //add restaurant calcs
-		//calculated at end of day: dailyFixedHotelCosts + dailyFixedRestaurantCosts + staffSalary(numStaff)
+	public double dayCosts(int numStaff) {
+		/**
+		 * Calculates the day's costs
+		 * fixed hotel costs
+		 * fixed restaurant costs
+		 * staff compensation
+		 */
 		double dayCosts = this.dailyFixedHotelCosts + this.dailyFixedRestaurantCosts + (this.staffSalary * numStaff);
 		return dayCosts;
 	}
 	
 	public double calculateCurrentAccountBalance(double dayRevenue, double dayCosts, boolean calledToday) {
-		if(calledToday == false) {
+		/**
+		 * calculates and updates the current balance 
+		 * add profit or subtract loss (if profit is negative)
+		 */
+		if(calledToday == false) {//avoid updates to balance multiple times in one day tick
 			this.currentAccountBalance = this.currentAccountBalance + dayRevenue - dayCosts;
 		}
 		return this.currentAccountBalance;
 	}
-	
-	public double getCurrentAccountBalance() { 
-		return currentAccountBalance;
-	}
-	
-	
-	/**
-	 * for final analysis at the end
-	 */
+
 	
 	public void calcExplicitCosts(int numStaff, int numDays) { 
+		/**
+		 * Calculates total explicit costs at the end of the simulation
+		 * explicit costs include total hotel, restaurant, and staff costs/compensation 
+		 */
 		this.totalStaffComp = this.staffSalary * numStaff * numDays;
 		this.totalHotelCosts = this.dailyFixedHotelCosts * numDays;
 		this.totalRestaurantCosts = this.dailyFixedRestaurantCosts * numDays;
@@ -68,11 +75,21 @@ public class FinancialAnalysis {
 	
 	
 	public void calcTotalRevenue(double initialMoney) { 
+		/**
+		 * calculates total revenue from all sources
+		 * total hotel room sales
+		 * total restaurant sales
+		 */
 		this.totalRevenue = this.currentAccountBalance - initialMoney + this.explicitCosts;
 	}
 
 	
-	public void financialOverview(int numStaff, int numDays, int totalGuests, int totalCustomers) { //not done
+	public void financialOverview(int numStaff, int numDays, int totalGuests, int totalCustomers) {
+		/**
+		 * calculates profit
+		 * prints hotel overview nicely
+		 * prints financial overview nicely
+		 */
 		this.profit = this.totalRevenue - this.explicitCosts;
 		System.out.println("\r\n " + "***Overview of Hotel Opporations***");
 		System.out.println("Total Hotel Guests: " + totalGuests);
